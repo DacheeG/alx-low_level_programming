ac9a814c4b4f73cee3f21a0ad8ae18fdb75ded51
DacheeG <Dacheegonfaa@gmail.com>
2023-02-16 11:41:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+(
+          printf("size of a char: %lu byte($)\n", sizeof(char));
+	  printf("size of a char: %lu byte($)\n", sizeof(char));
+	  printf("size of a char: %lu byte($)\n", sizeof(char));
+	  printf("size of a char: %lu byte($)\n", sizeof(char));
+	  printf("size of a char: %lu byte($)\n", sizeof(char));
+	  return (0)
+)
