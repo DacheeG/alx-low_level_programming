12758c5b937031995e308e5786f9cac3a8c8b528
DacheeG <Dacheegonfaa@gmail.com>
2023-02-17 04:00:31 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 char c = 'a';
 while (c <= 'z')
 {
-putchar(c);
-c++;
-}
-c = 'A';
-while (c <= 'z')
+if (c != 'e' && c != 'q')
 {
 putchar(c);
+}
 c++;
+}
+putchar('\n');
+return (0);
 }
