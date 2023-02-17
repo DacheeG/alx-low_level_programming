197b98e6a5edc8e0068eeb694ad9e728cb07c874
DacheeG <Dacheegonfaa@gmail.com>
2023-02-17 01:36:59 -0800

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - prints if the number is positive, negative or zero
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
@@ -12,16 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+		return (0);
 }
