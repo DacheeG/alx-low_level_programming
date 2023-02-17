6be9907f51a658ec045dd1ba78d07799db25ed6f
DacheeG <Dacheegonfaa@gmail.com>
2023-02-17 05:30:05 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, follow by a new line
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
