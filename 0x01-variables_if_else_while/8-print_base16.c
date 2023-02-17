87c3520d02c32f2a0134eb5da0793bb5a78c2a45
DacheeG <Dacheegonfaa@gmail.com>
2023-02-17 06:03:51 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-char ch;
-for (ch = '0'; ch <= '9'; ch++)
+char c;
+for (c = '0'; c <= '9'; c++)
 {
-putchar(ch);
+putchar(c);
 }
-for (ch = 'a'; ch <= 'f'; ch++)
+for (c = 'a'; c <= 'f'; c++)
 {
 putchar(c);
 }
