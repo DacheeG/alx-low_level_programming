078ab7a8ec2ae69abf26134eb9a27ba747a88f51
DacheeG <Dacheegonfaa@gmail.com>
2023-02-18 02:29:37 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ printf("size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("size ofa long long int: %d byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float:%d byte(s)\n", (unsigned long)sizeof(f));
+printf("size of float: %d byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
