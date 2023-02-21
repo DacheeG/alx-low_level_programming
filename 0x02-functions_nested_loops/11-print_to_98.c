717ac643a17c3c1a856882463e419d988cb91ce5
DacheeG <Dacheegonfaa@gmail.com>
2023-02-21 15:33:02 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,7 @@
  * print _to_98 - print all numbers from input to 98
  * Description:  ll natural numbers from n to 98
  * @n: the starting number
+ * Return: 0 or 1
  */
 void print_to_98(int n)
 {
