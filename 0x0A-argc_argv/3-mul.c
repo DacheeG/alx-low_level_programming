d3d71855e1c31f51ad766560743958fffdc8f537
DacheeG <Dacheegonfaa@gmail.com>
2023-03-11 02:04:02 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,47 +1,23 @@
 #include <stdio.h>
-		
 #include "main.h"
-		
-
-		
 /**
-		
- * _atoi - converts a string to an integer
-		
- * @s: string to be converted
-		
- *
-		
- * Return: the int converted from the string
-		
- */
-		
+* _atoi - converts a string to an integes
+* @s: string to be converted
+*
+* Return: the int converted from the string
+*/
 int _atoi(char *s)
-		
 {
-		
 	int i, d, n, len, f, digit;
-		
 
-		
 	i = 0;
-		
 	d = 0;
-		
 	n = 0;
-		
 	len = 0;
-		
 	f = 0;
-		
 	digit = 0;
-		
-
-		
 	while (s[len] != '\0')
-		
 		len++;
-		
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -61,20 +37,22 @@ int _atoi(char *s)
 	}
 	if (f == 0)
 		return (0);
-	return (n);		
+	return (n);
 }
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
 {
+
 	int result, num1, num2;
+
 	if (argc < 3 || argc > 3)
-	{	
+	{
 		printf("Error\n");
 		return (1);
 	}
@@ -82,5 +60,5 @@ int main(int argc, char *argv[])
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
-	return (0);		
+	return (0);
 }
