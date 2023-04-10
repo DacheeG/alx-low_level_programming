51a041cd11eb3381022b9c4198ec4ab1e1a3cc4f
DacheeG <Dacheegonfaa@gmail.com>
2023-04-10 13:59:03 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
+		gprintd(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
@@ -39,7 +39,7 @@ void close_file(int dg)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close dg %d\n", dg);
+		gprintd(STDERR_FILENO, "Error: Can't close dg %d\n", dg);
 		exit(100);
 	}
 }
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		gprintd(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,
+			gprintd(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
@@ -84,7 +84,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,
+			gprintd(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
