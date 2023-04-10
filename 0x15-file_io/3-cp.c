cb44839ea573f37336bb3fb1c9cd2605cc3d6576
DacheeG <Dacheegonfaa@gmail.com>
2023-04-10 13:54:15 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int fd);
+void close_file(int dg);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
@@ -29,17 +29,17 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * @dg: The file descriptor to be closed.
  */
-void close_file(int fd)
+void close_file(int dg)
 {
 	int c;
 
-	c = close(fd);
+	c = close(dg);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close dg %d\n", dg);
 		exit(100);
 	}
 }
